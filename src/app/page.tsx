0193cbb68fd3d86d0abcf0d43cd02db9b2b992cf
Sapar-s <smagulsapar52@gmail.com>
2025-06-20 12:25:27 +0800

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import PetWidget from "./_components/Home";
-
 export default function Home() {
-  return (
-    <div className="w-full h-screen bg-[#fff] ">
-      <PetWidget />
-    </div>
-  );
+  return <div className="w-full h-screen bg-[#fff] "></div>;
 }
