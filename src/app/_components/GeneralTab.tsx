89c9e58323f6648837fca75fa11e8067986570d9
Sapar-s <smagulsapar52@gmail.com>
2025-06-20 14:23:32 +0800

    settings

--- src/app/_components/GeneralTab.tsx
+++ src/app/_components/GeneralTab.tsx
@@ -1,10 +1,11 @@
 import { DialogContent } from "@/components/ui/dialog";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Settings } from "./Settings";
 
 export default function GeneralTab() {
   return (
     <DialogContent className="sm:max-w-[906px] w-full h-[618px]">
-      <Tabs defaultValue="account">
+      <Tabs defaultValue="account" className="flex flex-col gap-8 ">
         <TabsList className="grid w-full grid-cols-4">
           <TabsTrigger value="Question">Question</TabsTrigger>
           <TabsTrigger value="Buddy">Buddy</TabsTrigger>
@@ -14,7 +15,9 @@ export default function GeneralTab() {
         <TabsContent value="Question"></TabsContent>
         <TabsContent value="Buddy"></TabsContent>
         <TabsContent value="Todo"></TabsContent>
-        <TabsContent value="Settings"></TabsContent>
+        <TabsContent value="Settings">
+          <Settings />
+        </TabsContent>
       </Tabs>
     </DialogContent>
   );
--- src/app/_components/Settings.tsx
+++ src/app/_components/Settings.tsx
@@ -3,20 +3,20 @@ import React from "react";
 
 export const Settings = () => {
   return (
-    <div className="max-w-[800px] w-full h-[490px] flex flex-col gap-8 items-start ">
+    <div className="w-full h-[490px] flex flex-col gap-8 items-start ">
       <h4 className="text-[16px] font-[400] text-[#7F7F7F] ">
         Мэдэгдлийн тохиргоо
       </h4>
-      <div className="w-full h-[400px] flex flex-col gap-5 py-6 px-5 items-start rounded-[12px] border bg-[#F2F2F2] ">
-        <div className="w-full flex justify-between items-center border-b ">
+      <div className="w-full  flex flex-col gap-5 py-6 px-5 items-start rounded-[12px] border bg-[#F2F2F2] ">
+        <div className="w-full flex justify-between items-center border-b pb-5 ">
           <h4 className="text-[16px] font-[400]  ">Хийх зүйлсийн мэдэгдэл</h4>
           <Switch />
         </div>
-        <div className="w-full flex justify-between items-center border-b ">
+        <div className="w-full flex justify-between items-center border-b pb-5 ">
           <h4 className="text-[16px] font-[400]  ">Mood check ups</h4>
           <Switch />
         </div>
-        <div className="w-full flex justify-between items-center border-b ">
+        <div className="w-full flex justify-between items-center border-b pb-5 ">
           <h4 className="text-[16px] font-[400]  ">Санал хураалт</h4>
           <Switch />
         </div>
